ea016a35e2a9b757d6c89c79353f95e39ed9f5a9
ielbadisy <elbadisyimad@gmail.com>
2026-08-18 02:03:22 +0100

    Route complete() row-binding through data.table; return data.table

--- R/complete.R
+++ R/complete.R
@@ -3,11 +3,9 @@ complete.mimar_imputation <- function(x, action = 1, ...) {
   if (is.character(action)) {
     action <- match.arg(action, c("all", "long"))
     if (identical(action, "all")) return(x$imputations)
-    out <- do.call(rbind, lapply(seq_along(x$imputations), function(i) {
+    return(.rbind_or_empty(lapply(seq_along(x$imputations), function(i) {
       cbind(.imp = i, x$imputations[[i]], row.names = NULL)
-    }))
-    row.names(out) <- NULL
-    return(.as_tibble(out))
+    })))
   }
 
   if (!is.numeric(action) || length(action) != 1 || is.na(action)) {
@@ -17,5 +15,5 @@ complete.mimar_imputation <- function(x, action = 1, ...) {
   if (action < 1 || action > length(x$imputations)) {
     .mimar_stop("`action` must select an imputation between 1 and ", length(x$imputations), ".")
   }
-  .as_tibble(x$imputations[[action]])
+  .as_dt(x$imputations[[action]])
 }
